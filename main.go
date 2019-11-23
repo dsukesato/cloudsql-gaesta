681527db38305c000cc5c13d37ee9e6b8fb403b7
daisuke sato <satodsuke@gmail.com>
2019-11-24 01:56:49 +0900

    defaultからマルチプレクサに変更

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	}
 
 	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		log.Fatal(err)
 	}
 }
